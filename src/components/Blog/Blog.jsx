5327d49f12ed9eed5c0f8208dafa7040b062524f
mitrofanzxc <mitrofanzxc@gmail.com>
2022-02-19 13:41:23 +0300

    feat: add first shared object to blog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -13,15 +13,15 @@ const Blog = () => {
             <button className="button__all">See All</button>
           </div>
           <ul className="blog__slider">
-            {BlogInfo.map((BlogInfoCard) => {
+            {BlogInfo.map((BlogInfoItem) => {
               return (
                 <li className="blog__slider__item">
                   <div className="blog__slider__item__title">
                     <h3 className="h3">
-                      {BlogInfoCard.blog__slider__item__header}
+                      {BlogInfoItem.blog__slider__item__header}
                     </h3>
                     <div className="blog__slider__item__text">
-                      {BlogInfoCard.blog__slider__item__text}
+                      {BlogInfoItem.blog__slider__item__text}
                     </div>
                   </div>
                 </li>
