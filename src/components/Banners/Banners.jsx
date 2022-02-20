fa77d09e2e3804d66cc95b385a7f5ea73a99dd3b
mitrofanzxc <mitrofanzxc@gmail.com>
2022-02-21 00:26:24 +0300

    feat: add some adaptive for header and add some links

--- src/components/Banners/Banners.jsx
+++ src/components/Banners/Banners.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
 import "./Banners.scss";
 
 const Banners = () => {
@@ -9,16 +10,20 @@ const Banners = () => {
         <div className="inner__container">
           <ul className="banners__section__container">
             <li className="banners__section__container__item">
-              <div className="banners__section__container__item__title">
-                <h3 className="h3">New Season</h3>
-                <h2 className="h2">Lookbook Collection</h2>
-              </div>
+              <button className="banners__section__container__item__title">
+                <Link to="/women">
+                  <h3 className="h3">New Season</h3>
+                  <h2 className="h2">Lookbook Collection</h2>
+                </Link>
+              </button>
             </li>
             <li className="banners__section__container__item">
-              <div className="banners__section__container__item__title">
-                <h3 className="h3">Sale</h3>
-                <h2 className="h2">Get Up To 50% Off</h2>
-              </div>
+              <button className="banners__section__container__item__title">
+                <Link to="/sale">
+                  <h3 className="h3">Sale</h3>
+                  <h2 className="h2">Get Up To 50% Off</h2>
+                </Link>
+              </button>
             </li>
           </ul>
         </div>
