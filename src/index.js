2497c348a6bb6f1952c3e5e6674504993caf1e2e
mitrofanzxc <mitrofanzxc@gmail.com>
2022-03-11 01:28:18 +0300

    feat: add scroll to top

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
+
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, useLocation } from "react-router-dom";
+
 import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>,
