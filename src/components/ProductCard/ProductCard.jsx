edcea7c6745e0457252c310b2e127558f9dc03c4
mitrofanzxc <mitrofanzxc@gmail.com>
2022-03-11 22:03:17 +0300

    feat: add dynamics for clicking elements for productpage

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -265,9 +265,6 @@ const ProductCard = (props) => {
                 <div className="additional__information__list__property additional__text">
                   <h4 className="h4">Size:</h4>
                   {ProductSizes.map((ProductSize) => {
-                    const sizeArray = [];
-                    sizeArray.push(ProductSize);
-                    console.log(sizeArray);
                     return <p className="paragraph">{`${ProductSize},`}</p>;
                   })}
                 </div>
