9a67a043ca0b990265ec2328f49061942fd632e0
mitrofanzxc <mitrofanzxc@gmail.com>
2022-03-12 02:10:26 +0300

    feat: test

--- src/components/CatalogMain/CatalogMain.jsx
+++ src/components/CatalogMain/CatalogMain.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Link } from "react-router-dom";
 
 import { PRODUCTS } from "../../shared/Products";
@@ -8,6 +10,50 @@ import STAR_EMPTY from "./assets/img/star.svg";
 import "./CatalogMain.scss";
 
 const CatalogMain = (props) => {
+  const Category = props.category;
+  const NewArrivals = () => {
+    PRODUCTS[Category].slice(0, 8).map((PRODUCTSitem) => {
+      const RATING = PRODUCTSitem.rating;
+      const RATING_STARS_FILL = (
+        <img
+          src={STAR_FILL}
+          alt="star"
+          className="product__card__rating__item"
+        />
+      );
+      const RATING_STARS_EMPTY = (
+        <img
+          src={STAR_EMPTY}
+          alt="star"
+          className="product__card__rating__item"
+        />
+      );
+      const ARR_FILL = Array(5)
+        .fill(RATING_STARS_EMPTY)
+        .fill(RATING_STARS_FILL, 0, RATING);
+      const [newArrivals, setNewArrivals] = useState(NewArrivals);
+      return (
+        <>
+          <div className="product__card">
+            <Link to={`${PRODUCTSitem.category}/${PRODUCTSitem.id}`}>
+              <img
+                src={`https://training.cleverland.by/shop${PRODUCTSitem.images[0].url}`}
+                className="product__card__image"
+                alt="Product Card Men Img"
+              />
+            </Link>
+            <div className="product__card__title">{PRODUCTSitem.name}</div>
+            <div className="product__card__price__list">
+              <div className="product__card__price__item">
+                {`$ ${PRODUCTSitem.price.toFixed(2)}`}
+              </div>
+              <div className="product__card__rating">{ARR_FILL}</div>
+            </div>
+          </div>
+        </>
+      );
+    });
+  };
   return (
     <>
       <section className="catalog">
@@ -15,13 +61,19 @@ const CatalogMain = (props) => {
           <div className="catalog__header">
             <h2 className="h2">{`${props.category}'s`}</h2>
             <div className="catalog__filter">
-              <div className="catalog__filter__item">New Arrivals</div>
+              <div
+                className="catalog__filter__item"
+                onClick={() => setNewArrivals(NewArrivals)}
+              >
+                New Arrivals
+              </div>
               <div className="catalog__filter__item">Specials</div>
               <div className="catalog__filter__item">Bestsellers</div>
               <div className="catalog__filter__item">Most Viewed</div>
               <div className="catalog__filter__item">Feautured Products</div>
             </div>
           </div>
+          <div className="products__container">{newArrivals}</div>
           <div className="products__container">
             {PRODUCTS[props.category].slice(0, 8).map((PRODUCTSitem) => {
               const RATING = PRODUCTSitem.rating;
@@ -43,24 +95,26 @@ const CatalogMain = (props) => {
                 .fill(RATING_STARS_EMPTY)
                 .fill(RATING_STARS_FILL, 0, RATING);
               return (
-                <div className="product__card">
-                  <Link to={`${PRODUCTSitem.category}/${PRODUCTSitem.id}`}>
-                    <img
-                      src={`https://training.cleverland.by/shop${PRODUCTSitem.images[0].url}`}
-                      className="product__card__image"
-                      alt="Product Card Men Img"
-                    />
-                  </Link>
-                  <div className="product__card__title">
-                    {PRODUCTSitem.name}
-                  </div>
-                  <div className="product__card__price__list">
-                    <div className="product__card__price__item">
-                      {`$ ${PRODUCTSitem.price.toFixed(2)}`}
+                <>
+                  <div className="product__card">
+                    <Link to={`${PRODUCTSitem.category}/${PRODUCTSitem.id}`}>
+                      <img
+                        src={`https://training.cleverland.by/shop${PRODUCTSitem.images[0].url}`}
+                        className="product__card__image"
+                        alt="Product Card Men Img"
+                      />
+                    </Link>
+                    <div className="product__card__title">
+                      {PRODUCTSitem.name}
                     </div>
-                    <div className="product__card__rating">{ARR_FILL}</div>
+                    <div className="product__card__price__list">
+                      <div className="product__card__price__item">
+                        {`$ ${PRODUCTSitem.price.toFixed(2)}`}
+                      </div>
+                      <div className="product__card__rating">{ARR_FILL}</div>
+                    </div>
                   </div>
-                </div>
+                </>
               );
             })}
           </div>
